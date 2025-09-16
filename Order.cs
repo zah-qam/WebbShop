db561400482aa49749a4dc719bd517aaedd9590b
zahqa <zahqam.97@outlook.com>
2025-09-16 17:00:03 +0200

    Skapar klasser Order.cs, Product.cs, User.cs, WebbShopDbContext.cs. och skriver koden innuti dem.

---
+++ Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShop
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string Status { get; set; } = "Ny";
+        public DateTime OrderDate { get; internal set; }
+    }
+}
---
+++ WebbShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using WebbShop;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WebbShop
+{
+    internal class WebbShopDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        
+        public WebbShopDbContext(DbContextOptions<WebbShopDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory) // This extension method is in Microsoft.Extensions.Configuration.FileExtensions
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+    }
+}

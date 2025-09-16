b2af1d8514e0b6c9c12fa57cad6ab4be373b710a
zahqa <zahqam.97@outlook.com>
2025-09-16 18:00:38 +0200

    -Laddade ner BCrypt nugetPackage. -Skapade PasswprdHelper.cs med koden innuti den.

---
+++ PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShop
+{
+    internal class PasswordHelper
+    {
+        // genererar en ny hash med salt
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        // jämför ett vanligt lösenord med hash från databasen
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
